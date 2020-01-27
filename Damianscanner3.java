0f69fd23ffbd89c0c56ee3de474da5da10c1e1c0
cameliadamian <60077036+cameliadamian@users.noreply.github.com>
2020-01-27 22:32:47 +0200

    Add files via upload

---
+++ Damianscanner3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Damianscanner3 {
+    public static void main(String[] args) {
+Scanner qwer = new Scanner(System.in);
+System.out.println("Introduceti cantitatea de fructe uscate:");
+double m=qwer.nextByte();
+System.out.println("Introduceti procentajul de pierdere a mesei:");
+double p=qwer.nextByte();
+double x=(100*m)/(100-p);
+System.out.println("Cantitatea de fructe necesara:"+x);
+qwer.close();
+}
+}
