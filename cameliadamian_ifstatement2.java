ae83082dd5bf8c47ee574e2f7d8e327f68bcffd8
cameliadamian <60077036+cameliadamian@users.noreply.github.com>
2020-02-03 21:16:41 +0200

    Add files via upload

---
+++ cameliadamian_ifstatement2.java
@@ -0,0 +1,10 @@
+
+public class cameliadamian_ifstatement2 {
+	public static void main(String[] args) {
+		int R=3,A=6;
+		if(2*R==A)
+			System.out.println("Cercul incape in patrat");
+		else System.out.println("Cercul nu incape in patrat");
+		}
+
+}
