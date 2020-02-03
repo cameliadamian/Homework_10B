ca8404a697b8370978642fd2a509de1b49e3bdd6
cameliadamian <60077036+cameliadamian@users.noreply.github.com>
2020-02-03 20:50:44 +0200

    Add files via upload

---
+++ cameliadamian_ifstatement.java
@@ -0,0 +1,12 @@
+
+public class cameliadamian_ifstatement {
+public static void main(String[] args) {
+	int a=4,b=6;
+	int c=a*a,d=a+b;
+	if (a>0){
+		a=c;b=d;}
+	else {
+	 a=d;b=c;}
+	System.out.println("a="+a+",b="+b);
+}
+}
